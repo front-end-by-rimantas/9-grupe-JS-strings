b61ddf65fe469fc960b8e2818b97b1fa7ba6c0a5
Rimantas <belauzas@yahoo.com>
2019-07-25 10:17:24 +0300

    Templating in progress

--- js/html.js
+++ js/html.js
@@ -1,25 +1,42 @@
 "use strict";
 
-const services = [ {title: 'a'}, {title: 'b'}, {title: 'c'} ];
+const services = [ {title: 'service a'}, {title: 'service b'}, {title: 'service c'} ];
 const serviceHTML = `<div class="service">
                         <h3>title</h3>
                     </div>`;
 
-const testimonials = [ {text: 'a'}, {text: 'b'}, {text: 'c'} ];
+const testimonials = [ {text: 'testimonial a'}, {text: 'testimonial b'}, {text: 'testimonial c'} ];
 const testimonialHTML = `<div class="testimonial">
                             <h3>text</h3>
                         </div>`;
 
-const footerIcons = [ {icon: 'facebook', link: '#'},
-                      {icon: 'twitter', link: '#'},
-                      {icon: 'instagram', link: '#'} ];
-const footerIconsHTML = `<a href=link class="fa fa-icon"></a>`;
+const footerIcons = [ {icon: 'facebook', link: '#facebook'},
+                      {icon: 'twitter', link: '#twitter'},
+                      {icon: 'instagram', link: '#instagram'} ];
+const footerIconsHTML = `<a href=link class="fa fa-icon">Link</a>`;
+
+const batai = [ {size: 42, color: 'red', brand: 'niiike'},
+                {size: 44, color: 'white', brand: 'adike'},
+                {size: 39, color: 'random', brand: 'rabok'}];
+const bataiHTML = `<div class="batas">
+                        <h3>brand</h3>
+                        <span>size</span>
+                        <span>color</span>
+                    </div>`;
 
 function generate( target, data, html_template ){
-    let HTML = '';
+    let HTML = '',
+        template = '';
+    const keywords = Object.keys( data[0] );
 
     data.forEach( item => {
-        HTML += `html_template <--> ${item}`;
+        template = html_template;
+        
+        keywords.forEach( keyword => {
+            template = template.replace( keyword, item[keyword] );
+        });
+        
+        HTML += template;
     });
 
     return document.querySelector(target).innerHTML = HTML;
@@ -28,3 +45,4 @@ function generate( target, data, html_template ){
 generate('#services_block', services, serviceHTML);
 generate('#testimonials_block', testimonials, testimonialHTML);
 generate('#footer', footerIcons, footerIconsHTML);
+generate('#batai', batai, bataiHTML);
